39743d03bcb65454e159406a219a63e818f78161
LostBear <101864136+BearPlush@users.noreply.github.com>
2022-03-18 10:16:56 +0100

    Update Main.java

--- dwwm/main/Main.java
+++ dwwm/main/Main.java
@@ -11,12 +11,12 @@ public class Main {
 		 #						    D A T A                             #
 		 ################################################################							
 		 */
-		int user_code = 0000; // RentrÈ du code de l'utilisateur
-		int code_verif = 3345; // Chiffre dans la base de donnÈe code verif
+		int user_code = 0000; // Rentr√© du code de l'utilisateur
+		int code_verif = 3345; // Chiffre dans la base de donn√©e code verif
 		int atm_fund = 10000; // Argent restant dispo dans l'ATM
 		int user_fund = 10; // Argent disponnible sur le compte de l'utilisateur
 		int atm_ticket = 500; // Nombre de ticket restant dans l'ATM
-		int check = 0; // Variable qui va servir de donnÈe temporaire pour le montant
+		int check = 0; // Variable qui va servir de donn√©e temporaire pour le montant
 		int out_u = 0; // nous permettra de stocker si il y a un code erreur
 		short cpt = 3; // Compteur de tentative (max 3)
 		/**
@@ -24,26 +24,33 @@ public class Main {
 		 #					     P R O G R A M                          #
 		 ################################################################							
 		 */
-		Scanner sc = new Scanner(System.in);																		 //On dit que l'on voudra que l'utilisateur Ècris qque chose
+		Scanner sc = new Scanner(System.in);				//On dit que l'on voudra que l'utilisateur √©cris qque chose
 
-		while (cpt > 0) {																							//Tant que notre compteur est supÈrireur a 0
-			Sortie.avec_code(Sortie.ok.DEMANDE);																		//Appel de la fonction Sortie.avec_code de ma OK.DEMANDE
-			try {																										//on essaie de faire
-				user_code = Integer.parseInt(sc.nextLine());																//TransformÈ la saisie utilisateur en int que l'on met dans user_code
-				if (user_code == code_verif)																				//SI le code rentrÈ est le bon 
-					break;																									//alors on stop ce block de program
-			} catch (Exception e) {																						//on essaie de voir si il y a une erreur
-			} finally {																									//Finalement quoi qui ce passe (si le program ne c pas fais arretter) 
-				cpt -= 1;																									//On rajoute +1 a compteur de tentative
+		while (cpt > 0) {						//Tant que notre compteur est sup√©rireur a 0
+			Sortie.avec_code(Sortie.ok.DEMANDE);				//Appel de la fonction Sortie.avec_code de ma OK.DEMANDE
+			try {									//on essaie de faire
+				user_code = Integer.parseInt(sc.nextLine());				//Transform√© la saisie utilisateur en int que l'on met dans user_code
+				if (user_code == code_verif)						//SI le code rentr√© est le bon 
+					break;									//alors on stop ce block de program
+			} catch (Exception e) {							//on essaie de voir si il y a une erreur
+			} finally {								//Finalement quoi qui ce passe (si le program ne c pas fais arretter)
+				cpt -= 1;								//On rajoute +1 a compteur de tentative
 			}
 			
 		}
-		if (cpt > 0) {																								//Si le compteur est supÈrieur a 0																							
-			try {																										//on essaie de faire
-				Withdraw.withdraw(user_code, code_verif, atm_fund, user_fund, atm_ticket, check, out_u);					//apelle de la fonction Withdraw
-			} catch (InterruptedException e) {																			//si il y a une erreur on rÈcupËre l'erreur
+		if (cpt > 0) {							//Si le compteur est sup√©rieur a 0																							
+			try {								//on essaie de faire
+				Withdraw.withdraw(						//apelle de la fonction Withdraw
+					user_code,
+					code_verif,
+					atm_fund,
+					user_fund,
+					atm_ticket,
+					check,
+					out_u);	
+			} catch (InterruptedException e) {				//si il y a une erreur on r√©cup√®re l'erreur
 				// TODO Auto-generated catch block
-				e.printStackTrace();																						//On affiche l'erreur
+				e.printStackTrace();						//On affiche l'erreur
 			}
 		}
 	}//Fin static void
