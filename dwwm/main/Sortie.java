9768f05e6d6277d852e16277c3c6f58f12869b4d
LostBear <101864136+BearPlush@users.noreply.github.com>
2022-03-18 10:41:10 +0100

    Update Sortie.java

--- dwwm/main/Sortie.java
+++ dwwm/main/Sortie.java
@@ -22,19 +22,19 @@ public class Sortie {
 	public static void avec_code(Sortie.ko out_u) {												 //SI Sortie.avec_code = Sortie.ko
 		switch (out_u) {															//Switch
 			case MONTANT: 																//SI out_u = MONTANT
-				System.out.println("Nous ne donnons de la monaie que par : 10€ 20€ ou 50€ merci de rentrer un montant valide");				//Afficher cette ligne
+				System.out.println("Nous ne donnons de la monaie que par : 10€ 20€ ou 50€ merci de rentrer un montant valide");				//Afficher texte et sauter la ligne
 				break;																	//Fin du si 
 			case CODE: 																//SI out_u = CODE
-				System.out.println("Code invalide");													//Afficher cette ligne
+				System.out.println("Code invalide");													//Afficher texte et sauter la ligne
 				break;																	//Fin du si 
 			case FONDS:																//SI out_u = FONDS
-				System.out.println("L'ATM ne posséde plus les fonds nessecaire. Merci pour votre compréhension");					//Afficher cette ligne
+				System.out.println("L'ATM ne posséde plus les fonds nessecaire. Merci pour votre compréhension");					//Afficher texte et sauter la ligne
 				break;																	//Fin du si 
 			case PAUVRE:																//SI out_u = PAUVRE
-				System.out.println("Vous n'avez plus assez d'argent sur votre compte bancaire pour effectué ce retrait.");				//Afficher cette ligne
+				System.out.println("Vous n'avez plus assez d'argent sur votre compte bancaire pour effectué ce retrait.");				//Afficher texte et sauter la ligne
 				break;																	//Fin du si 
 			case TICKET:																//SI out_u = TICKETS
-				System.out.println("Vous ne pouvez pas avoir de recue il ne reste plus de ticket.");							//Afficher cette ligne
+				System.out.println("Vous ne pouvez pas avoir de recue il ne reste plus de ticket.");							//Afficher texte et sauter la ligne
 				break;																	//Fin du si 
 		}//fin de switch
 	}//fin function Sortie avec_code KO
@@ -42,20 +42,20 @@ public class Sortie {
 	public static void avec_code(Sortie.ok out_u) {												//SI Sortie.avec_code = Sortie.ok
 		switch(out_u) {																//Switch
 			case CODE:																//SI out_u = CODE
-				System.out.println("Code Validé !");													//Afficher cette ligne
+				System.out.println("Code Validé !");													//Afficher texte et sauter la ligne
 				break;																	//Fin du si 
 			case VOULUE:																//SI out_u = VOULUE
-				System.out.println("Combien souhaitez vous retirer ?");											//Afficher cette ligne
+				System.out.println("Combien souhaitez vous retirer ?");											//Afficher texte et sauter la ligne
 				break;																	//Fin du si
 			case TRANSACTION:															//SI out_u = TRANSACTION
 				try {																	//Essaie de
-					System.out.print("Transaction en cours");												//Afficher cette ligne
+					System.out.print("Transaction en cours");												//Afficher texte
 					Thread.sleep(1000);															//attendre 1 seconde
-					System.out.print(".");															//Afficher cette ligne
+					System.out.print(".");															//Afficher texte
 					Thread.sleep(1000);															//attendre 1 seconde
-					System.out.print(".");															//Afficher cette ligne
+					System.out.print(".");															//Afficher texte
 					Thread.sleep(1000);															//attendre 1 seconde
-					System.out.println(". ");														//Afficher cette ligne
+					System.out.println(". ");														//Afficher texte et sauter la ligne
 					Thread.sleep(1000);															//attendre 1 seconde
 				} catch (InterruptedException e) {													//on récupére l'erreur si y'a une erreur
 					// TODO Auto-generated catch block
@@ -63,16 +63,16 @@ public class Sortie {
 				}
 				break;																		//Fin du si
 			case RESTE:																//SI out_u = RESTE
-				System.out.print("Il vous reste actuellement : ");											//Afficher cette ligne
+				System.out.print("Il vous reste actuellement : ");											//Afficher texte
 				break;																	//Fin du si
 			case RETRAIT:																//SI out_u = RETRAIT
-				System.out.println("Veuillez récuperer votre argent.");											//Afficher cette ligne
+				System.out.println("Veuillez récuperer votre argent.");											//Afficher texte et sauter la ligne
 				break;																	//Fin du si
 			case MERCI:																//SI out_u = MERCI
-				System.out.println("Merci pour votre fidélité !");											//Afficher cette ligne
+				System.out.println("Merci pour votre fidélité !");											//Afficher texte et sauter la ligne
 				break;																	//Fin du si
 			case DEMANDE:																//SI out_u = DEMANDE
-				System.out.print("Veuillez rentrer votre code : ");											//Afficher cette ligne
+				System.out.print("Veuillez rentrer votre code : ");											//Afficher texte
 				break;																	//Fin du si
 			default: break;																//Par défaut Fin du switch
 		}//fin de switch
