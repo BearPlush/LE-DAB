b38d8e4c73fe3b2a513074f67ff0097bd53ccd0f
LostBear <101864136+BearPlush@users.noreply.github.com>
2022-03-18 10:36:06 +0100

    Update Sortie.java

--- dwwm/main/Sortie.java
+++ dwwm/main/Sortie.java
@@ -44,10 +44,10 @@ public class Sortie {
 			case CODE:																//SI out_u = CODE
 				System.out.println("Code Validé !");													//Afficher cette ligne
 				break;																	//Fin du si 
-			case VOULUE:																//SI out_u = CODE
+			case VOULUE:																//SI out_u = VOULUE
 				System.out.println("Combien souhaitez vous retirer ?");											//Afficher cette ligne
 				break;																	//Fin du si
-			case TRANSACTION:															//SI out_u = CODE
+			case TRANSACTION:															//SI out_u = TRANSACTION
 				try {																	//Essaie de
 					System.out.print("Transaction en cours");												//Afficher cette ligne
 					Thread.sleep(1000);															//attendre 1 seconde
@@ -62,16 +62,16 @@ public class Sortie {
 					e.printStackTrace();															//on affiche l'erreur
 				}
 				break;																		//Fin du si
-			case RESTE:																//SI out_u = CODE
+			case RESTE:																//SI out_u = RESTE
 				System.out.print("Il vous reste actuellement : ");											//Afficher cette ligne
 				break;																	//Fin du si
-			case RETRAIT:																//SI out_u = CODE
+			case RETRAIT:																//SI out_u = RETRAIT
 				System.out.println("Veuillez récuperer votre argent.");											//Afficher cette ligne
 				break;																	//Fin du si
-			case MERCI:																//SI out_u = CODE
+			case MERCI:																//SI out_u = MERCI
 				System.out.println("Merci pour votre fidélité !");											//Afficher cette ligne
 				break;																	//Fin du si
-			case DEMANDE:																//SI out_u = CODE
+			case DEMANDE:																//SI out_u = DEMANDE
 				System.out.print("Veuillez rentrer votre code : ");											//Afficher cette ligne
 				break;																	//Fin du si
 			default: break;																//Par défaut Fin du switch
